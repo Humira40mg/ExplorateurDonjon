2a64cb69403185a4ed25a5f65730fe98ae88dd76
FanderFir2875 <nguyenalexandre700@gmail.com>
2025-05-23 10:02:30 +1100

    update des objets

--- donjon_objet/Piege.java
+++ donjon_objet/Piege.java
@@ -1,3 +1,23 @@
 public class Piege {
-    
+    private String type;
+    private int degats;
+    private String skin = "(X)";
+
+    public Piege(String type, int degats) {
+        this.type = type;
+        this.degats = degats;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getDegats() {
+        return degats;
+    }
+
+    @Override
+    public String toString() {
+        return "Piège: " + type + " (Dégâts: " + degats + ") " + skin;
+    }
 }
--- donjon_objet/Tresor.java
+++ donjon_objet/Tresor.java
@@ -1,5 +1,20 @@
-public class Tresor_composite {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Tresor
+{
+    private int valeur_score;
+    private String skin = "[-o-]";
+
+    public Tresor(int valeur_score) {
+        this.valeur_score = valeur_score;
     }
+
+    public int getValeur_score() {
+        return valeur_score;
+    }
+
+    @Override
+    public String toString() {
+        return "( Trésor avec Valeur: " + valeur_score + ") " + skin;
+    }
+
 }
+
