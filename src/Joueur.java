f85fed28c946a1921ef4a783e4d5349242be8d3f
Humira40mg <nathanmillot@icloud.com>
2025-05-23 09:36:35 +1100

    Objet Joueur

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,3 +1,62 @@
+/**
+ * Classe singleton representant le joueur.
+ */
 public class Joueur
 {
+    private static Joueur instance;
+    private int pv;
+    private int score;
+    public String skin = "('u')"; //apparence visuelle du joueur
+
+    public Joueur()
+    {
+        pv = 100;
+        score = 0;
+    }
+
+    /**
+     * @return L'instance unique du Joueur
+     */
+    public static Joueur getInstance();
+    {
+        if (instance == null)
+        {
+            instance = new Joueur();
+        }
+        return instance;
+    }
+
+    /**
+     * Fait perdre de la vie au joueur (il en a 100 de base).
+     *
+     * @param damage (le nombre de degat prit)
+     * @return true si le joueur n'a plus de points de vie.
+     */
+    public boolean takeDamage(int damage)
+    {
+        pv -= damage;
+        if (pv <= 0) return true;
+        return false;
+    }
+
+    /**
+     * incremente de x le score du joueur
+     *
+     * @param x un entier positif
+     */
+    public void incrementScore(int x)
+    {
+        score += x;
+    }
+
+    //getters
+    public int getPv()
+    {
+        return pv;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
 }
