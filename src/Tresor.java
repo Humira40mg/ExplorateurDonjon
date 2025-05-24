5f5f56c61a933d0367f27e8b0e7dd7b998561c9a
FanderFir2875 <nguyenalexandre700@gmail.com>
2025-05-24 19:24:41 +1100

    ajout des commentaires pours tresors
    commentaires pour tresors

--- src/Tresor.java
+++ src/Tresor.java
@@ -1,21 +1,52 @@
+/**
+ * Représente un trésor qui peut être ramassé par le joueur.
+ * 
+ * @author NGUYEN Alexandre et Nathan Milllot
+ */
 public class Tresor extends ElementDonjon {
+    /**
+     * Valeur du trésor en terme de score.
+     */
     private int valeur_score;
+    /**
+     * Symbole qui représente le trésor sur la carte.
+     */
     private String skin = "[-o-]";
 
+    /**
+     * Constructeur initialisant le trésor avec une valeur de score.
+     * 
+     * @param valeur_score La valeur du trésor en terme de score.
+     */
     public Tresor(int valeur_score) {
         this.valeur_score = valeur_score;
     }
-
+    
+    /**
+     * Retourne la valeur du trésor.
+     * 
+     * @return La valeur du trésor.
+     */
     public int getValeur_score() {
         return valeur_score;
     }
-
+    
+    /**
+     * Lorsque le joueur touche le trésor, il ramasse le trésor et gagne le score correspondant.
+     * 
+     * @param joueur Le joueur qui a touché le trésor.
+     */
     @Override
     public void touch(Joueur joueur) {
         if(!joueur.isAlive())return; 
         joueur.incrementScore(valeur_score);
     }
-
+    
+    /**
+     * Retourne une chaîne de caractère représentant le trésor.
+     * 
+     * @return Une chaîne de caractère représentant le trésor.
+     */
     @Override
     public String toString() {
         return estActif()
