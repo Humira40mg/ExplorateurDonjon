b4ddb156140f0fa1a99cbc344965cc554b68933b
FanderFir2875 <nguyenalexandre700@gmail.com>
2025-05-25 22:43:18 +1100

    ajoute les commentaires

--- src/GenerateurSalle.java
+++ src/GenerateurSalle.java
@@ -1,20 +1,36 @@
+/**
+ * Classe qui génère une salle avec des éléments aléatoires
+ */
 public class GenerateurSalle extends Salle {
-    private static Salle salle;
+    private static Salle salle; // instance de la salle courante
 
-    // Define an explicit constructor that calls the Salle class's constructor
+    /**
+     * Constructeur qui appelle le constructeur de la classe Salle
+     * @param elem groupe d'éléments
+     * @param matrice matrice de la salle
+     */
     public GenerateurSalle(GroupeDonjon elem, char[][] matrice) {
-        super(elem, matrice); // Call the Salle class's constructor
+        super(elem, matrice); // Appelle le constructeur de la classe Salle
     }
 
+    /**
+     * Retourne la salle courante, si elle n'existe pas, en crée une nouvelle
+     * @param elem groupe d'éléments
+     * @param matrice matrice de la salle
+     * @return la salle courante
+     */
     public static Salle getSalle(GroupeDonjon elem, char[][] matrice) {
-        if(salle ==null){
-            //  creer une nouvelle salle avec les parametres par defaut ou chargés 
+        if(salle == null){
+            // Crée une nouvelle salle avec les paramètres par défaut ou chargés 
             salle = new Salle(elem, matrice);
         }
         return salle;
     }
 
+    /**
+     * Détruit la salle courante
+     */
     public static void destroySalle() {
-        salle = null; // temporaire a changer si on pense que il y a a changer 
+        salle = null; // temporaire à changer si on pense que il y a à changer 
     }
 }
